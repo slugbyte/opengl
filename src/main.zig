c89784d543c506b7651b2c1c8bba5e3d89eeb283
Duncan Marsh <slugbyte@slugbyte.com>
2025-04-11 00:58:33 +0800

    wip batch render triangles

--- src/main.zig
+++ src/main.zig
@@ -5,57 +5,118 @@
 const std = @import("std");
 const c = @import("./c.zig");
 
+const VertexBuffer = std.ArrayList(f32);
+
 const basic_vertex_shader_source = @embedFile("./shader/basic.vertex.glsl");
 const solid_fragment_shader_source = @embedFile("./shader/red.fragment.glsl");
 // 1) create a triangle using pixelspace that is solid color
 // 2) make a gradient using vector interpolation
 var renderer: Renderer = undefined;
 
+pub const Color = struct {
+    r: u8 = 0,
+    g: u8 = 0,
+    b: u8 = 0,
+
+    const White = Color{
+        .r = 255,
+        .g = 255,
+        .b = 255,
+    };
+
+    const Blue = Color{
+        .r = 0,
+        .g = 0,
+        .b = 255,
+    };
+
+    const Green = Color{
+        .r = 0,
+        .g = 255,
+        .b = 0,
+    };
+};
+
 pub const Renderer = struct {
     u_window: c_int,
     window_width: c_int = 0,
     window_height: c_int = 0,
-
-    shape_vao: c_uint,
-    shape_vbo: c_uint,
+    vao: c_uint,
+    vbo: c_uint,
     fill_shader: c_uint,
+    vertex_count: u32 = 0,
+    vertex_buffer: VertexBuffer,
 
-    pub fn init() !Renderer {
-        var shape_vao: c_uint = undefined;
-        var shape_vbo: c_uint = undefined;
-        c.glCreateVertexArrays(1, &shape_vao);
-        c.glGenBuffers(1, &shape_vbo);
+    pub fn init(allocator: std.mem.Allocator) !Renderer {
+        var vao: c_uint = undefined;
+        var vbo: c_uint = undefined;
+        c.glCreateVertexArrays(1, &vao);
+        c.glGenBuffers(1, &vbo);
 
-        c.glBindVertexArray(shape_vao);
-        c.glBindBuffer(c.GL_ARRAY_BUFFER, shape_vbo);
-        c.glVertexAttribPointer(0, 2, c.GL_FLOAT, c.GL_FALSE, 2 * @sizeOf(f32), @ptrCast(&0));
+        c.glBindVertexArray(vao);
+        c.glBindBuffer(c.GL_ARRAY_BUFFER, vbo);
+        // aPos x, y
+        c.glVertexAttribPointer(0, 2, c.GL_FLOAT, c.GL_FALSE, 5 * @sizeOf(f32), @ptrFromInt(0));
         c.glEnableVertexAttribArray(0);
+        // aColor rgb
+        c.glVertexAttribPointer(1, 3, c.GL_FLOAT, c.GL_FALSE, 5 * @sizeOf(f32), @ptrFromInt(2 * @sizeOf(f32)));
+        c.glEnableVertexAttribArray(1);
 
         const fill_shader = try shader_program_create(basic_vertex_shader_source, solid_fragment_shader_source);
         const u_window = c.glGetUniformLocation(fill_shader, "u_window");
 
         return .{
-            .shape_vao = shape_vao,
-            .shape_vbo = shape_vbo,
+            .vao = vao,
+            .vbo = vbo,
             .fill_shader = fill_shader,
             .u_window = u_window,
+            .vertex_buffer = VertexBuffer.init(allocator),
         };
     }
 
     pub fn deinit(self: *Renderer) void {
-        c.glDeleteBuffers(1, &self.shape_vbo);
-        c.glDeleteVertexArrays(1, &self.shape_vao);
+        c.glDeleteBuffers(1, &self.vbo);
+        c.glDeleteVertexArrays(1, &self.vao);
         c.glDeleteProgram(self.fill_shader);
     }
 
+    pub fn draw_rect(self: *Renderer, x: i32, y: i32, width: i32, height: i32, color: Color) !void {
+        const x0: f32 = @floatFromInt(x);
+        const y0: f32 = @floatFromInt(y);
+        const x1: f32 = @floatFromInt(x + width);
+        const y1: f32 = @floatFromInt(y + height);
+
+        const r: f32 = @floatFromInt(color.r);
+        const g: f32 = @floatFromInt(color.g);
+        const b: f32 = @floatFromInt(color.b);
+        const vertex_data: [30]f32 = .{
+            x0, y0, r, g, b,
+            x1, y0, r, g, b,
+            x0, y1, r, g, b,
+            x0, y1, r, g, b,
+            x1, y0, r, g, b,
+            x1, y1, r, g, b,
+        };
+
+        try self.vertex_buffer.appendSlice(&vertex_data);
+        self.vertex_count += 6;
+    }
+
     pub fn begin(self: *Renderer) void {
+        self.vertex_count = 0;
         c.glUseProgram(self.fill_shader);
-        c.glBindVertexArray(self.shape_vao);
-        c.glBindBuffer(c.GL_ARRAY_BUFFER, self.shape_vbo);
+        c.glBindVertexArray(self.vao);
+        c.glBindBuffer(c.GL_ARRAY_BUFFER, self.vbo);
         c.glUniform2f(self.u_window, @floatFromInt(self.window_width), @floatFromInt(self.window_height));
     }
 
-    pub fn end(_: *Renderer) void {
+    pub fn end(self: *Renderer) void {
+        c.glBindBuffer(c.GL_ARRAY_BUFFER, self.vbo);
+        c.glBufferData(c.GL_ARRAY_BUFFER, @intCast(self.vertex_buffer.items.len * @sizeOf(f32)), @ptrCast(self.vertex_buffer.items.ptr), c.GL_DYNAMIC_DRAW);
+        c.glDrawArrays(c.GL_TRIANGLES, 0, @intCast(self.vertex_count));
+        self.vertex_buffer.resize(0) catch {
+            @panic("failed to resize");
+        };
         c.glUseProgram(0);
         c.glBindVertexArray(0);
     }
@@ -124,28 +185,6 @@ fn framebuffer_resize_callback(_: ?*c.GLFWwindow, width: c_int, height: c_int) c
     std.debug.print("window: {d}w {d}h\n", .{ width, height });
 }
 
-pub fn draw_rect(x: f32, y: f32, width: f32, height: f32) void {
-    const x0 = x;
-    const y0 = y;
-    const x1 = x + width;
-    const y1 = y + height;
-
-    const vertex_data: [12]f32 = .{
-        x0, y0,
-        x1, y0,
-        x0, y1,
-
-        x0, y1,
-        x1, y0,
-        x1, y1,
-    };
-
-    renderer.begin();
-    renderer.vertex_data_set(vertex_data[0..]);
-    c.glDrawArrays(c.GL_TRIANGLES, 0, 6);
-    renderer.end();
-}
-
 const DVD = struct {
     const dvd_width = 50;
     const dvd_height = 50;
@@ -156,7 +195,7 @@ const DVD = struct {
     x_direction: f32 = 250,
     y_direction: f32 = 250,
 
-    fn update(self: *DVD, dt: f32) void {
+    pub fn update(self: *DVD, dt: f32) void {
         const x_border: f32 = @as(f32, @floatFromInt(renderer.window_width));
         const y_border: f32 = @as(f32, @floatFromInt(renderer.window_height));
 
@@ -171,18 +210,25 @@ const DVD = struct {
 
         self.x = self.x + (dt * self.x_direction);
         self.y = self.y + (dt * self.y_direction);
+    }
 
-        // if (self.x + dvd_width > @as(f32, @floatFromInt(renderer.window_width)) or self.x < 0) {
-        //     self.x = 0;
-        // }
-
-        // if (self.y + dvd_height > @as(f32, @floatFromInt(renderer.window_height)) or self.y < 0) {
-        //     self.y = 0;
-        // }
+    pub fn render(self: *DVD) void {
+        renderer.draw_rect(@intFromFloat(self.x), @intFromFloat(self.y), @intFromFloat(DVD.dvd_width), @intFromFloat(DVD.dvd_height), Color.Green) catch {
+            @panic("unable to draw rect");
+        };
+        renderer.draw_rect(@intFromFloat(self.x + 200), @intFromFloat(self.y + 200), @intFromFloat(DVD.dvd_width), @intFromFloat(DVD.dvd_height), Color.White) catch {
+            @panic("unable to draw rect");
+        };
+        renderer.draw_rect(@intFromFloat(self.x + 200), @intFromFloat(self.y + 50), @intFromFloat(DVD.dvd_width), @intFromFloat(DVD.dvd_height), Color.Blue) catch {
+            @panic("unable to draw rect");
+        };
     }
 };
 
 pub fn main() !void {
+    var debug_allocator = std.heap.DebugAllocator(.{}){};
+    const allocator = debug_allocator.allocator();
+
     var win_width: c_int = undefined;
     var win_height: c_int = undefined;
     // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
@@ -207,7 +253,7 @@ pub fn main() !void {
     }
     c.glfwMakeContextCurrent(window);
 
-    renderer = try Renderer.init();
+    renderer = try Renderer.init(allocator);
 
     c.glViewport(0, 0, 800, 600);
     _ = c.glfwSetFramebufferSizeCallback(window, &framebuffer_resize_callback);
@@ -231,7 +277,7 @@ pub fn main() !void {
 
         renderer.begin();
         dvd.update(delta_time);
-        draw_rect(dvd.x, dvd.y, DVD.dvd_width, DVD.dvd_height);
+        dvd.render();
         renderer.end();
 
         c.glfwSwapBuffers(window);
--- src/shader/basic.vertex.glsl
+++ src/shader/basic.vertex.glsl
@@ -1,8 +1,12 @@
 #version 330 core
 
 layout (location = 0) in vec2 aPos;
+layout (location = 1) in vec3 aColor;
+
 uniform vec2 u_window;
+out vec3 vColor;
 
 void main() {
     gl_Position = vec4((aPos / u_window * 2.0 - 1.0) * vec2(1.0, -1.0), 0.0, 1.0);
+    vColor = aColor;
 }
